ab0e1362d060eb65ceac506acb6794a129cb78ad
maa123 <maa123.lq@gmail.com>
2016-08-30 00:51:15 +0900

    add code

--- src/http.js
+++ src/http.js
@@ -1,3 +1,12 @@
-connect.get=function("url"){
-
+connect.get=function(url){
+	var httpGet = new org.apache.http.client.methods.HttpPost(url);
+	var httpClient = new org.apache.http.impl.client.DefaultHttpClient();
+	httpGet.setHeader("Connection", "Keep-Alive");
+	var response = httpClient.execute(httpGet);
+	var status = response.getStatusLine().getStatusCode();
+	if(status != org.apache.http.HttpStatus.SC_OK){
+		return false;
+	}else{
+		return org.apache.http.util.EntityUtils.toString(response.getEntity(), "UTF-8");
+	}
 };
