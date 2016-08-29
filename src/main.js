44530e174eb18c40bf00f54c067116f0e4c5cd81
maa123 <maa123.lq@gmail.com>
2016-08-29 20:31:26 +0900

    ちょっと短く

--- src/main.js
+++ src/main.js
@@ -6,103 +6,29 @@ var i = 0;
     	i=(i+1)|0;
 	}
 i=null;
-function addHook(hookname,callback){
-	switch(hookname){
-		case "attackHook":
+function addHook(hookname, callback){
+	if(hooknames.indexOf(hookname)>=0){
 		hooks[hookname].push(callback);
 		return true;
-		break;
-		case "chatHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "continueDestroyBlock":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "destroyBlock":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "projectileHitEntityHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "eatHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "entityAddedHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "entityHurtHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "entityRemovedHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "explodeHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "serverMessageReceiveHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "deathHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "playerAddExpHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "playerExpLevelChangeHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "redstoneUpdateHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "screenChangeHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "newLevel":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "startDestroyBlock":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "projectileHitBlockHook":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "modTick":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		case "useItem":
-		hooks[hookname].push(callback);
-		return true;
-		break;
-		default:
+	}else{
 		return false;
-		break;
 	}
 }
+
 
 // can use preventDefault()
 function attackHook(attacker, victim){
 	var i = 0;
 	while(i < hooks["attackHook"].length) {
     	hooks["attackHook"][i](attacker,victim);
+    	i=(i+1)|0;
+	}
+}
+// can use preventDefault()
+function chatHook(str){
+	var i = 0;
+	while(i < hooks["chatHook"].length) {
+    	hooks["chatHook"][i](str);
     	i=(i+1)|0;
 	}
 }
