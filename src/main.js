6bd01224123644228784aa4ba53a505c4fa0b060
maa123 <maa123.lq@gmail.com>
2016-08-29 20:48:24 +0900

    変数名変更

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
-var hooks={};
-var hooknames=["attackHook","chatHook","continueDestroyBlock","destroyBlock","projectileHitEntityHook","eatHook","entityAddedHook","entityHurtHook","entityRemovedHook","explodeHook","serverMessageReceiveHook","deathHook","playerAddExpHook","playerExpLevelChangeHook","redstoneUpdateHook","screenChangeHook","newLevel","startDestroyBlock","projectileHitBlockHook","modTick","useItem"];
+var sml.name="ScriptMODLoader";
+var sml.version=1;
+var sml.hooks={};
+var sml.hooknames=["attackHook","chatHook","continueDestroyBlock","destroyBlock","projectileHitEntityHook","eatHook","entityAddedHook","entityHurtHook","entityRemovedHook","explodeHook","serverMessageReceiveHook","deathHook","playerAddExpHook","playerExpLevelChangeHook","redstoneUpdateHook","screenChangeHook","newLevel","startDestroyBlock","projectileHitBlockHook","modTick","useItem"];
 var i = 0;
-	while(i < hooknames.length) {
-    	hooks[hooknames[i]]=[];
+	while(i < sml.hooknames.length) {
+    	sml.hooks[sml.hooknames[i]]=[];
     	i=(i+1)|0;
 	}
 i=null;
 function addHook(hookname, callback){
-	if(hooknames.indexOf(hookname)>=0){
-		hooks[hookname].push(callback);
+	if(sml.hooknames.indexOf(hookname)>=0){
+		sml.hooks[hookname].push(callback);
 		return true;
 	}else{
 		return false;
@@ -19,159 +21,159 @@ function addHook(hookname, callback){
 // can use preventDefault()
 function attackHook(attacker, victim){
 	var i = 0;
-	while(i < hooks["attackHook"].length) {
-    	hooks["attackHook"][i](attacker,victim);
+	while(i < sml.hooks["attackHook"].length) {
+    	sml.hooks["attackHook"][i](attacker,victim);
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function chatHook(str){
 	var i = 0;
-	while(i < hooks["chatHook"].length) {
-    	hooks["chatHook"][i](str);
+	while(i < sml.hooks["chatHook"].length) {
+    	sml.hooks["chatHook"][i](str);
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function continueDestroyBlock(x, y, z, side, progress){
 	var i = 0;
-	while(i < hooks["continueDestroyBlock"].length) {
-    	hooks["continueDestroyBlock"][i](x,y,z,side,progress);
+	while(i < sml.hooks["continueDestroyBlock"].length) {
+    	sml.hooks["continueDestroyBlock"][i](x,y,z,side,progress);
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function destroyBlock(x, y, z, side){
 	var i = 0;
-	while(i < hooks["destroyBlock"].length) {
-    	hooks["destroyBlock"][i](x,y,z,side);
+	while(i < sml.hooks["destroyBlock"].length) {
+    	sml.hooks["destroyBlock"][i](x,y,z,side);
     	i=(i+1)|0;
 	}
 }
 function projectileHitEntityHook(projectile, targetEntity){
 	var i = 0;
-	while(i < hooks["projectileHitEntityHook"].length) {
-    	hooks["projectileHitEntityHook"][i](projectile,targetEntity);
+	while(i < sml.hooks["projectileHitEntityHook"].length) {
+    	sml.hooks["projectileHitEntityHook"][i](projectile,targetEntity);
     	i=(i+1)|0;
 	}
 }
 function eatHook(hearts, saturationRatio){
 	var i = 0;
-	while(i < hooks["eatHook"].length) {
-    	hooks["eatHook"][i](hearts,saturationRatio);
+	while(i < sml.hooks["eatHook"].length) {
+    	sml.hooks["eatHook"][i](hearts,saturationRatio);
     	i=(i+1)|0;
 	}
 }
 function entityAddedHook(entity){
 	var i = 0;
-	while(i < hooks["entityAddedHook"].length) {
-    	hooks["entityAddedHook"][i](entity);
+	while(i < sml.hooks["entityAddedHook"].length) {
+    	sml.hooks["entityAddedHook"][i](entity);
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function entityHurtHook(attacker, victim, halfhearts){
 	var i = 0;
-	while(i < hooks["entityHurtHook"].length) {
-    	hooks["entityHurtHook"][i](attacker,victim,halfhearts);
+	while(i < sml.hooks["entityHurtHook"].length) {
+    	sml.hooks["entityHurtHook"][i](attacker,victim,halfhearts);
     	i=(i+1)|0;
 	}
 }
 function entityRemovedHook(entity){
 	var i = 0;
-	while(i < hooks["entityRemovedHook"].length) {
-    	hooks["entityRemovedHook"][i](entity);
+	while(i < sml.hooks["entityRemovedHook"].length) {
+    	sml.hooks["entityRemovedHook"][i](entity);
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function explodeHook(entity, x, y, z, power, onFire){
 	var i = 0;
-	while(i < hooks["explodeHook"].length) {
-    	hooks["explodeHook"][i](entity,x,y,z,power,onFire);
+	while(i < sml.hooks["explodeHook"].length) {
+    	sml.hooks["explodeHook"][i](entity,x,y,z,power,onFire);
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function serverMessageReceiveHook(str){
 	var i = 0;
-	while(i < hooks["serverMessageReceiveHook"].length) {
-    	hooks["serverMessageReceiveHook"][i](str);
+	while(i < sml.hooks["serverMessageReceiveHook"].length) {
+    	sml.hooks["serverMessageReceiveHook"][i](str);
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function deathHook(attacker, victim){
 	var i = 0;
-	while(i < hooks["deathHook"].length) {
-    	hooks["deathHook"][i](attacker,victim);
+	while(i < sml.hooks["deathHook"].length) {
+    	sml.hooks["deathHook"][i](attacker,victim);
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function playerAddExpHook(player, experienceAdded){
 	var i = 0;
-	while(i < hooks["playerAddExpHook"].length) {
-    	hooks["playerAddExpHook"][i](player,experienceAdded);
+	while(i < sml.hooks["playerAddExpHook"].length) {
+    	sml.hooks["playerAddExpHook"][i](player,experienceAdded);
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function playerExpLevelChangeHook(player, levelsAdded){
 	var i = 0;
-	while(i < hooks["playerExpLevelChangeHook"].length) {
-    	hooks["playerExpLevelChangeHook"][i](player,levelsAdded);
+	while(i < sml.hooks["playerExpLevelChangeHook"].length) {
+    	sml.hooks["playerExpLevelChangeHook"][i](player,levelsAdded);
     	i=(i+1)|0;
 	}
 }
 function redstoneUpdateHook(x, y, z, newCurrent, someBooleanIDontKnow, blockId, blockData){
 	var i = 0;
-	while(i < hooks["redstoneUpdateHook"].length) {
-    	hooks["redstoneUpdateHook"][i](x,y,z,newCurrent,someBooleanIDontKnow,blockid,blockData);
+	while(i < sml.hooks["redstoneUpdateHook"].length) {
+    	sml.hooks["redstoneUpdateHook"][i](x,y,z,newCurrent,someBooleanIDontKnow,blockid,blockData);
     	i=(i+1)|0;
 	}
 }
 function screenChangeHook(screenName){
 	var i = 0;
-	while(i < hooks["screenChangeHook"].length) {
-    	hooks["screenChangeHook"][i](screenName);
+	while(i < sml.hooks["screenChangeHook"].length) {
+    	sml.hooks["screenChangeHook"][i](screenName);
     	i=(i+1)|0;
 	}
 }
 function newLevel(){
 	var i = 0;
-	while(i < hooks["newLevel"].length) {
-    	hooks["newLevel"][i]();
+	while(i < sml.hooks["newLevel"].length) {
+    	sml.hooks["newLevel"][i]();
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function startDestroyBlock(x, y, z, side){
 	var i = 0;
-	while(i < hooks["startDestroyBlock"].length) {
-    	hooks["startDestroyBlock"][i](x,y,z,side);
+	while(i < sml.hooks["startDestroyBlock"].length) {
+    	sml.hooks["startDestroyBlock"][i](x,y,z,side);
     	i=(i+1)|0;
 	}
 }
 function projectileHitBlockHook(projectile, blockX, blockY, blockZ, side){
 	var i = 0;
-	while(i < hooks["projectileHitBlockHook"].length) {
-    	hooks["projectileHitBlockHook"][i](projectile,blockX,blockY,blockZ,side);
+	while(i < sml.hooks["projectileHitBlockHook"].length) {
+    	sml.hooks["projectileHitBlockHook"][i](projectile,blockX,blockY,blockZ,side);
     	i=(i+1)|0;
 	}
 }
 function modTick(){
 	var i = 0;
-	while(i < hooks["modTick"].length) {
-    	hooks["modTick"][i]();
+	while(i < sml.hooks["modTick"].length) {
+    	sml.hooks["modTick"][i]();
     	i=(i+1)|0;
 	}
 }
 // can use preventDefault()
 function useItem(x, y, z, itemid, blockid, side, itemDamage, blockDamage){
 	var i = 0;
-	while(i < hooks["useItem"].length) {
-    	hooks["useItem"][i](x,y,z,itemid,blockid,side,itemDamage,blockDamage);
+	while(i < sml.hooks["useItem"].length) {
+    	sml.hooks["useItem"][i](x,y,z,itemid,blockid,side,itemDamage,blockDamage);
     	i=(i+1)|0;
 	}
 }
